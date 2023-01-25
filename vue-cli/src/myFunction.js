4e538d5b282c529712fd7e2804d907eb1f4ab1e7
hjRyu <ryu8701@gmail.com>
2023-01-25 16:56:50 +0900

    Feat: 스크롤 기능 완성
    스크롤시 활성탭 색 변화하는 기능 완성

--- vue-cli/src/myFunction.js
+++ vue-cli/src/myFunction.js
@@ -1,5 +1,14 @@
+//smooth 하게 스크롤
 exports.scrollToPosition=function (position){
     window.scroll({ top: position, behavior: "smooth" })
+}
+
+//scroll 방향 반환
+exports.scrollDirection=function(last){
+    const cur=window.scrollY
+    const direction=last<cur?'down':'up'
+    const lastScroll=cur;
+    return {lastScroll,direction}
 }
 // var topOfElement = document.querySelector('#targetElement').offsetTop - XX;
 // window.scroll({ top: topOfElement, behavior: "smooth" });
