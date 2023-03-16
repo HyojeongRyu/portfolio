8de227e714ba14b9ea7d1c356a5507de86dee3fc
HyojeongRyu <ryu8701@gmail.com>
2023-03-16 16:33:59 +0900

    merge

--- vue-cli/src/components/MyCareer.vue
+++ vue-cli/src/components/MyCareer.vue
@@ -1,10 +1,24 @@
 <template>
-  <div class="container-career">career</div>
+  <div class="container-career">
+    <h3>기타활동/이력</h3>
+    <ul>
+      <li><span class="card_title project">• 개발기간 </span><span>{{ card.period }}</span></li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return{
+      school:[
+        {date:'2018. 03. ~ 2018. 12. ',content: '부경대학교 동아리연합회장'},
+        {date:'2017. 09. ~ 2017. 12. ', content: 'NSU 한국문화교육봉사동아리 Secretary'},
+        {date:'2017. 09. ~ 2017. 12. ', content: 'NSU 한인학생회'},
+        {date:'2015. 03. ~ 2015. 12. ', content: '부경대학교 경영학부 학생회 사무국장'},
+      ]
+    }
+  }
 }
 </script>
 
